6aa1692d1490149e121635a00587883256080318
Sachin C <sachin1245@hotmail.com>
2016-05-19 17:42:51 +0530

    Added Custom message event

--- Public/js/app.js
+++ Public/js/app.js
@@ -3,3 +3,11 @@ var socket = io();
 socket.on('connect',function(){
 	console.log('Connected to socket.io Server!');
 });
+
+socket.on('message', function (message){
+	console.log(message.text);
+});
+
+socket.emit('message',{
+	text: 'Hi buddy how are you?'
+})
